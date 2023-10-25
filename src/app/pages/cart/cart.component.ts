712f0c100d97235845566be68cb3afa23dc2907d
minhng-178 <anhminh1782002@gmail.com>
2023-10-25 11:32:46 +0700

    cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,15 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Cart, CartItem } from "src/app/models/cart.model";
 
 @Component({
-  selector: 'app-cart',
-  template: `
-    <p>
-      cart works!
-    </p>
-  `,
-  styles: [
-  ]
+  selector: "app-cart",
+  templateUrl: "cart.component.html",
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  cart: Cart = {
+    items: [
+      {
+        product: "https://placehold.co/150",
+        name: "sneaker",
+        price: 150,
+        quantity: 1,
+        id: 1,
+      },
+      {
+        product: "https://placehold.co/150",
+        name: "sneaker",
+        price: 150,
+        quantity: 1,
+        id: 1,
+      },
+      {
+        product: "https://placehold.co/150",
+        name: "sneaker",
+        price: 150,
+        quantity: 1,
+        id: 1,
+      },
+    ],
+  };
 
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    "product",
+    "name",
+    "price",
+    "quantity",
+    "total",
+    "action",
+  ];
+
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items
+      .map((item) => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0);
+  }
 }
